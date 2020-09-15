11eb78a58053bd77165f822d70e74e4388307da8
Break-1T <taras.krupko07@gmail.com>
2020-09-15 20:49:09 +0300

    проверка

---
+++ MainController/2-nd lb classes/Invoice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainController._2_nd_lb_classes
+{
+    class Invoice
+    {
+        public Invoice(int account, string customer, string provider)
+        {
+            Account = account;
+            Customer = customer;
+            Provider = provider;
+        }
+
+        public int Account { get; private set; }
+        public string Customer { get; private set; }
+        public string Provider { get; private set; }
+
+        string _article;
+        int _quality;
+
+        const double tax = 20; // НДС - 20%
+
+        public void Price()
+        {
+
+        }
+
+    }
+}
